66240fea15e8ae280b6eb44d0a7ddc03d58b0f0a
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-12 22:42:47 +0530

    Mailbox
    Mailbox

--- src/index.js
+++ src/index.js
@@ -144,3 +144,20 @@ import "./styles.css";
 //   }
 // }
 // ReactDOM.render(<LoginControl />, document.getElementById("root"));
+// function Mailbox(props) {
+//   const unreadMessages = props.unreadMessages;
+//   return (
+//     <div>
+//       <h1>Hello!!!!!</h1>
+//       {unreadMessages.length > 0 && (
+//         <h2>You have {unreadMessages.length} unread messages.</h2>
+//       )}
+//     </div>
+//   );
+// }
+// const messages = ["a", "b", "c", "d"];
+
+// ReactDOM.render(
+//   <Mailbox unreadMessages={messages} />,
+//   document.getElementById("root")
+// );
