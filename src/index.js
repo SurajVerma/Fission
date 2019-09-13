fe6652bc280dfd87163759aca77b51affe165c0e
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-13 18:08:35 +0530

    reverted last change
    reverted last change

--- src/index.js
+++ src/index.js
@@ -161,34 +161,34 @@ import "./styles.css";
 //   <Mailbox unreadMessages={messages} />,
 //   document.getElementById("root")
 // );
-// function WarningBanner(props) {
-//   if (!props.warn) {
-//     return null;
-//   }
-//   return <div className="warning">Warning!</div>;
-// }
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+  return <div className="warning">Warning!</div>;
+}
 
-// class Page extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { showWarning: true };
-//     this.handleToggleClick = this.handleToggleClick.bind(this);
-//   }
-//   handleToggleClick() {
-//     this.setState(state => ({
-//       showWarning: !state.showWarning
-//     }));
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <WarningBanner warn={this.state.showWarning} />
-//         <button onClick={this.handleToggleClick}>
-//           {this.state.showWarning ? "Hide" : "Show"}
-//         </button>
-//       </div>
-//     );
-//   }
-// }
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showWarning: true };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? "Hide" : "Show"}
+        </button>
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render(<Page />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
