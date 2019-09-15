3b2b93d361a7a7034476af5f92168105987ff172
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-15 15:48:57 +0530

    list & keys
    list & keys

--- src/index.js
+++ src/index.js
@@ -161,34 +161,49 @@ import "./styles.css";
 //   <Mailbox unreadMessages={messages} />,
 //   document.getElementById("root")
 // );
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-  return <div className="warning">Warning!</div>;
-}
+// function WarningBanner(props) {
+//   if (!props.warn) {
+//     return null;
+//   }
+//   return <div className="warning">Warning!</div>;
+// }
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { showWarning: true };
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-  handleToggleClick() {
-    this.setState(state => ({
-      showWarning: !state.showWarning
-    }));
-  }
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? "Hide" : "Show"}
-        </button>
-      </div>
-    );
-  }
-}
+// class Page extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { showWarning: true };
+//     this.handleToggleClick = this.handleToggleClick.bind(this);
+//   }
+//   handleToggleClick() {
+//     this.setState(state => ({
+//       showWarning: !state.showWarning
+//     }));
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <WarningBanner warn={this.state.showWarning} />
+//         <button onClick={this.handleToggleClick}>
+//           {this.state.showWarning ? "Hide" : "Show"}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
-ReactDOM.render(<Page />, document.getElementById("root"));
+// ReactDOM.render(<Page />, document.getElementById("root"));
+
+//List and keys
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map(number => (
+    <li key={number.toString()}>{number}</li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+const numbers = [1, 2, 3, 4, 5, 6];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById("root")
+);
