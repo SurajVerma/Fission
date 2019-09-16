dbbda07bf2786cf71dd140bbb2e03d2cdf87c2aa
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-16 22:22:00 +0530

    Forms
    Forms

--- src/index.js
+++ src/index.js
@@ -291,3 +291,37 @@ import "./styles.css";
 // }
 
 // ReactDOM.render(<EssayForm />, document.getElementById("root"));
+
+// class FlavorForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: "mango" };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ value: event.target.value });
+//   }
+//   handleSubmit(event) {
+//     alert("Your favorite flavor is: " + this.state.value);
+//     event.preventDefault();
+//   }
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Pick your favorite flavor :
+//           <select onChange={this.handleChange} value={this.state.value}>
+//             <option value="grapefruit">Grapefruit</option>
+//             <option value="lime">lime</option>
+//             <option value="coconut">coconut</option>
+//             <option value="mango">mango</option>
+//           </select>
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<FlavorForm />, document.getElementById("root"));
