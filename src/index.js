aaa72485007658893cf6ffa546dc684b1c22a008
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-16 21:59:06 +0530

    Forms
    Input and textarea

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-import { func } from "prop-types";
 
 // function Welcome(props) {
 //   return <h1>Hello, {props.name}</h1>;
@@ -225,3 +224,70 @@ import { func } from "prop-types";
 //   <NumberList numbers={numbers} />,
 //   document.getElementById("root")
 // );
+
+//Forms
+// class NameForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: "" };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ value: event.target.value.toUpperCase() });
+//   }
+//   handleSubmit(event) {
+//     alert("A name was submitted: " + this.state.value);
+//     event.preventDefault();
+//   }
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name:
+//           <input
+//             type="text"
+//             value={this.state.value}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<NameForm />, document.getElementById("root"));
+
+// class EssayForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value:
+//         "Please write an essay about that random thing which pops in your mind right now"
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ value: event.target.value });
+//   }
+//   handleSubmit(event) {
+//     alert("An essay was submitted: " + this.state.value);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Essay:
+//           <textarea value={this.state.value} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<EssayForm />, document.getElementById("root"));
