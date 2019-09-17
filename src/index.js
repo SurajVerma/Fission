62858eb4ac2abbbe01f714c2bf72a4631ec4f204
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-17 19:09:47 +0530

    Finishing up
    Finishing up

--- src/index.js
+++ src/index.js
@@ -362,9 +362,52 @@ const scaleNames = {
   c: "Celsius",
   f: "Fahrenheit"
 };
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "" };
   }
-  render() {}
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
+  render() {
+    const temperature = this.state.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in </legend>
+        <input type="text" onChange={this.handleChange} value={temperature} />
+      </fieldset>
+    );
+  }
 }
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Calculator />, document.getElementById("root"));
