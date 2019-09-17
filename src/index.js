a1a15e10d6caadbd86dc44a2fc11bbc0cce08d38
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-17 15:48:09 +0530

    calculator
    calculator

--- src/index.js
+++ src/index.js
@@ -325,3 +325,35 @@ import "./styles.css";
 // }
 
 // ReactDOM.render(<FlavorForm />, document.getElementById("root"));
+
+//Lifting State Up
+
+// function BoilingVerdict(props) {
+//   if (props.celsius >= 100) {
+//     return <p>The water would boil.</p>;
+//   }
+//   return <p>The water would not boil.</p>;
+// }
+
+// class Calculator extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { temperature: "" };
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleChange(e) {
+//     this.setState({ temperature: e.target.value });
+//   }
+//   render() {
+//     const temperature = this.state.temperature;
+//     return (
+//       <fieldset>
+//         <legend>Enter temperature in Celsius</legend>
+//         <input type="text" value={temperature} onChange={this.handleChange} />
+//         <BoilingVerdict celsius={parseFloat(temperature)} />
+//       </fieldset>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Calculator />, document.getElementById("root"));
